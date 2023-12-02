fff4aba5eafb227d4276400b867e3e87b97dda79
Wan Ahmad Ikhwannaufal <139196434+ikhwannaufal@users.noreply.github.com>
2023-12-02 11:17:48 +0800

    Add files via upload

---
+++ logic.js
@@ -0,0 +1,11 @@
+// Before the game start
+// Listen for any keyboard action
+// Start the game
+
+// After the game start
+// - Jump dino when a space key is pressed 
+// - Listen for collision between dino and cactus
+// - If there is a collision, make the game end
+//  - If there is no collision update the score and generate obstacle
+
+// Game ends (Game over)
